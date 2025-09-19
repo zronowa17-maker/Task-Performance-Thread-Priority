b87181b2aa87a5a111b614bf16e0c32a5de3b9b2
zronowa17-maker <zronowa17@gmail.com>
2025-09-19 18:59:11 +0800

    Create a loop that loops two (2) times in Thread1

--- Task Performance Thread Priority/MyThreadClass.cs
+++ Task Performance Thread Priority/MyThreadClass.cs
@@ -9,9 +9,16 @@ namespace Task_Performance_Thread_Priority
 {
     public class MyThreadClass
     {
+        public static void Thread1()
+        {
+            for (int loopCount = 0; loopCount < 2; loopCount++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Name of Thread: " + Thread.CurrentThread.Name + " Process = " + loopCount);
+            }
+        }
     }
 }
-
 
 
      
